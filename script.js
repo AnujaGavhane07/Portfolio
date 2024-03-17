5b1d0df86adb3383fa8ab362def0915f8a869de8
AnujaGavhane07 <148804978+AnujaGavhane07@users.noreply.github.com>
2024-03-17 22:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Form submission
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    alert('Form submitted successfully!');
+});
